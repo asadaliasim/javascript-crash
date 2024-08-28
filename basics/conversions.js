5c99634072711170e638df80e6caba738025bc0f
Asad Ali Asim <asadaliasim909@gmail.com>
2024-08-28 15:29:25 +0500

    feat: basic conversion

---
+++ basics/conversions.js
@@ -0,0 +1,18 @@
+let score = '22';
+
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+
+//  "33" => 33
+//  "33sdd" => NaN
+// true => 1 ; false => 0
+
+let isLoggedIn = 0;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 => true, 0 => flase
+// "" => false
+// "anyValue" => true
