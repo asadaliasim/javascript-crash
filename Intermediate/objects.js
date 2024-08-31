d6001a1cf35f8cd24ab36ab2e2dc301e1081590d
Asad Ali Asim <asadaliasim909@gmail.com>
2024-08-31 20:51:37 +0500

    feat:objects in detail

--- Intermediate/objects.js
+++ Intermediate/objects.js
@@ -31,4 +31,35 @@ user.greeting = function () {
 user.greetingName = function () {
   console.log(`Hello ${this.name}`);
 };
-console.log(user.greetingName());
+// console.log(user.greetingName());
+
+// const tinderUser = new Object();
+const tinderUser = {};
+
+tinderUser.id = '123a';
+tinderUser.name = 'Asad';
+// console.log(tinderUser);
+
+const regularUser = {
+  email: 'asad@gmail.com',
+  fullName: {
+    userFullName: {
+      firstName: 'asad',
+      lastName: 'ali',
+    },
+  },
+};
+
+// console.log(regularUser.fullName?.userFullName.firstName);
+
+const obj1 = { 1: 'a', 2: 'b' };
+const obj2 = { 3: 'c', 4: 'e' };
+// const obj3 = Object.assign({}, obj1, obj2);
+
+// const obj3 = { ...obj1, ...obj2 };
+// console.log(obj3);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
