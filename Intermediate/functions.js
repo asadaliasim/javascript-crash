8ba91b59db1046e17b3a4e537659f7aeefc7e6b5
Asad Ali Asim <asadaliasim909@gmail.com>
2024-09-01 11:31:48 +0500

    feat:functions

---
+++ Intermediate/functions.js
@@ -0,0 +1,30 @@
+function sayMyName() {
+  console.log('A');
+  console.log('S');
+  console.log('A');
+  console.log('D');
+}
+
+// sayMyName();
+// parameters
+const addTwoNumbers = function (num1, num2) {
+  //   console.log(num1 + num2);
+
+  //   let result = num1 + num2;
+  //   return result;
+  return num1 + num2;
+};
+// arguments
+const result = addTwoNumbers(1, 2);
+
+// console.log(result);
+
+function loginMessage(username) {
+  if (!username) {
+    return `Enter username`;
+  }
+  return `${username} logged In`;
+}
+
+const res = loginMessage('');
+console.log(res);
