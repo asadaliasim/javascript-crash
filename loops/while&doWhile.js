8fac63be6d9a02cf432e6702e15145bba7ac18af
Asad Ali Asim <asadaliasim909@gmail.com>
2024-09-08 23:23:13 +0500

    feat:while and do while loop

---
+++ loops/while&doWhile.js
@@ -0,0 +1,32 @@
+// let index = 0
+// while (condition) {
+
+// index++
+// }
+
+// syntax
+// do {
+
+// } while (condition);
+
+// let index = 0;
+// while (index <= 10) {
+//   console.log(`Value of index is ${index}`);
+//   index = index + 2;
+// }
+
+// let myArr = ['asad', 'ali', 'asim'];
+// let arr = 0;
+// while (arr < myArr.length) {
+//   console.log(myArr[arr]);
+//   arr++;
+// }
+
+// do while
+
+// runs at least one time
+let score = 11;
+do {
+  console.log(`Score is ${score}`);
+  score++;
+} while (score < 5);
