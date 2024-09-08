94649e76f14d339ba73535d1ab7a6f4dbf40308d
Asad Ali Asim <asadaliasim909@gmail.com>
2024-09-08 23:52:39 +0500

    feat:for of and for in

---
+++ loops/highOrderFun.js
@@ -0,0 +1,53 @@
+// for of  - loop
+// can used to iterate strings
+// const arr = [1, 2, 3, 4, 5];
+// for (const num of arr) {
+//   console.log(num);
+// }
+
+const greetings = 'Hello World!';
+
+// for (const greet of greetings) {
+//   console.log(greet);
+// }
+
+// Maps
+const map = new Map();
+map.set('IN', 'India');
+map.set('US', 'United States');
+map.set('pk', 'Pakistan');
+
+// console.log(map);
+
+// for (const key of map) {
+// for (const [key, value] of map) {
+//   console.log(value);
+// }
+
+const games = {
+  game1: 'NFS',
+  game2: 'gta',
+};
+// objects are not iterable
+// for (const [key, value] of games) {
+//   console.log(value);
+// }
+
+// to iterate objs
+const myObj = {
+  js: 'Javascript',
+  cpp: 'C++',
+  rb: 'ruby',
+};
+
+// for (const key in myObj) {
+//   console.log(myObj[key]);
+//   console.log(key);
+// }
+
+const programming = ['js', 'php', 'java'];
+
+for (const key in programming) {
+  console.log(key);
+  console.log(programming[key]);
+}
